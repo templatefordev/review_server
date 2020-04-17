7d1af5a18019a1c282cce3658c9d7706053bc091
Evgeniy Azarov <azarov.private@gmail.com>
2020-04-17 18:36:25 +0300

    add base module for schema

---
+++ lib/review_server/schema/base.ex
@@ -0,0 +1,23 @@
+defmodule ReviewServer.Schema.Base do
+  @moduledoc """
+  Every schema file should use this module.
+
+  ## Examples
+      defmodule ReviewServer.Schema.Random
+        use ReviewServer.Schema.Base
+
+        # ...
+      end
+  """
+
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+
+      @primary_key {:id, :binary_id, read_after_writes: true}
+      @foreign_key_type :binary_id
+      @timestamps_opts [type: :utc_datetime_usec]
+    end
+  end
+end
